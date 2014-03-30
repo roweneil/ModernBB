23262040bfd818077bedeb9dc1295adc77f53ae9
roweneil <neilrowe39@gmail.com>
2014-03-30 17:50:39 +0100

    Open links in new tab

--- include/parser.php
+++ include/parser.php
@@ -711,7 +711,7 @@ function handle_url_tag($url, $link = '', $bbcode = false)
 		else
 			$link = stripslashes($link);
 
-		return '<a href="'.$full_url.'" rel="nofollow">'.$link.'</a>';
+		return '<a href="'.$full_url.'" rel="nofollow" target = "_blank">'.$link.'</a>';
 	}
 }
 
